65091a002d9ebe3210812b6b1b761269c692e862
Rahul Saxena <saxenarahul0101@gmail.com>
2026-04-07 15:02:25 +0530

    Home url added

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,10 @@ from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Hello, World!"
+
 @app.route("/submit",methods=["POST"])
 def submit():
     data = request.json
